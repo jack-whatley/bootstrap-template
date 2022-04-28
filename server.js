63eb05e91f13d93bc4ba2340ef7f58048eb6ba33
wheatley1239 <wickselm2005@gmail.com>
2022-04-28 15:12:11 +0100

    removed static modules

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
-app.use(express.static("node_modules"));
+// app.use(express.static("node_modules"));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
